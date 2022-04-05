6c9e8eaef2941e30a216bd4d0165244e35a80542
Ryan Miller <ryan@devopsmachine.com>
2022-04-05 15:51:17 -0400

    Use cluster name for output dir and ISO name

--- create-iso.sh
+++ create-iso.sh
@@ -5,8 +5,20 @@
 
 set -e
 
-if [[ -d ./ocp ]] ; then
-    >&2 echo './ocp exists from previous run. Remove it before running again!'
+if [[ ! -f ./install-config.yaml ]] ; then
+    >&2 echo "./install-config.yaml not found. See README.md for script usage."
+    exit 1
+fi
+
+# Hack to get cluster name from install-config.yaml
+CLUSTER_NAME=$(grep -A 1 'metadata:' install-config.yaml \
+                 | tail -n 1 \
+                 | cut -d ':' -f 2 \
+                 | xargs)
+
+if [[ -d ./$CLUSTER_NAME ]] ; then
+    >&2 echo "./$CLUSTER_NAME exists from previous run. Remove it before" \
+             "running again!"
     exit 1
 fi
 
@@ -34,25 +46,26 @@ fi
 # Download RHCOS ISO if it's not present
 if [[ ! -f ./rhcos.x86_64.iso ]] ; then
     echo "./rhcos.x86_64.iso not present; Downloading RHCOS image..."
-    curl -L -o rhcos.x86_64.iso "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/latest/rhcos-live.x86_64.iso"
+    curl -L -o rhcos.x86_64.iso \
+        "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/latest/rhcos-live.x86_64.iso"
 else
     echo "./rhcos.x86_64.iso present; Using existing RHCOS image"
 fi
 
 echo "Generating ignition config from install-config.yaml..."
-mkdir ocp
-cp install-config.yaml ocp
-./openshift-install --dir=ocp create single-node-ignition-config
+mkdir "$CLUSTER_NAME"
+cp install-config.yaml "$CLUSTER_NAME"
+./openshift-install --dir="$CLUSTER_NAME" create single-node-ignition-config
 
 echo "Copying RHCOS image..."
-cp rhcos.x86_64.iso ocp/custom_rhcos.x86_64.iso
+cp rhcos.x86_64.iso "${CLUSTER_NAME}/rhcos_${CLUSTER_NAME}.x86_64.iso"
 
 echo "Embedding igntion config into RHCOS image..."
 ./coreos-installer \
     iso \
     ignition \
     embed \
-    -fi ocp/bootstrap-in-place-for-live-iso.ign \
-    ocp/custom_rhcos.x86_64.iso
+    -fi "${CLUSTER_NAME}/bootstrap-in-place-for-live-iso.ign" \
+    "${CLUSTER_NAME}/rhcos_${CLUSTER_NAME}.x86_64.iso"
 
 echo "Complete!"
