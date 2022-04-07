680a8969bcc8c3210855f8f9403ec20428d94647
Ryan Miller <ryan@devopsmachine.com>
2022-04-07 09:42:11 -0400

    Swap install monitor command with bootstrap-complete

--- create-iso.sh
+++ create-iso.sh
@@ -78,4 +78,4 @@ echo "Installer Output (kubeconfig is here!): $OUTPUT_DIR"
 echo "ISO Path: ${OUTPUT_DIR}/rhcos_${CLUSTER_NAME}.x86_64.iso"
 echo
 echo "Boot machine using the ISO above. After booting, monitor progress with:"
-echo "$ ./openshift-install --dir=\"${OUTPUT_DIR}\" wait-for install-complete"
+echo "$ ./openshift-install --dir=\"${OUTPUT_DIR}\" wait-for bootstrap-complete"
